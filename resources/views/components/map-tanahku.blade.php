a255523a4abc51e2d72e1661572a4b3e84b2b046
Memet Zx <zxce3@gnuweeb.org>
2024-02-16 11:19:53 +0700

    Add map-tanahku component with styles and script
    Signed-off-by: Memet Zx <zxce3@gnuweeb.org>

---
+++ resources/views/components/map-tanahku.blade.php
@@ -0,0 +1,28 @@
+<div id="map" class="map"></div>
+
+<style>
+    #dot {
+        width: 10px;
+        height: 10px;
+        border: 1px solid #088;
+        border-radius: 5px;
+        background-color: #0FF;
+        opacity: 0.5;
+    }
+    .popover-body {
+        min-width: 276px;
+    }
+    /* map point */
+    .dot {
+        height: 10px;
+        width: 10px;
+        border-radius: 50%;
+        display: inline-block;
+    }
+</style>
+<script>
+    var devices = @json($devices);
+
+    var firstDevice = devices[0];
+</script>
+
