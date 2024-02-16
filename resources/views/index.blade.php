61db7225d6a2f320c8174278ad7661d56dc3c255
Memet Zx <zxce3@gnuweeb.org>
2024-02-16 11:16:35 +0700

    Add index.blade.php with map and styling
    Signed-off-by: Memet Zx <zxce3@gnuweeb.org>

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<x-app-layout>
+    <div class="card h-50 m-2">
+        <x-map-tanahku/>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+</x-app-layout>
+<style>
+    .popover-body {
+        min-width: 276px;
+    }
+    #marker {
+        width: 20px;
+        height: 20px;
+        border: 1px solid #088;
+        border-radius: 10px;
+        background-color: #0FF;
+        opacity: 0.5;
+      }
+      #vienna {
+        text-decoration: none;
+        color: white;
+        font-size: 11pt;
+        font-weight: bold;
+        text-shadow: black 0.1em 0.1em 0.2em;
+      }
+
+</style>
