ca7b6f4b2b342e8906c2cf5b9789afd0ca4888ff
Memet Zx <zxce3@gnuweeb.org>
2024-02-16 11:16:57 +0700

    Add Halfmoon CSS and initialize popovers
    Signed-off-by: Memet Zx <zxce3@gnuweeb.org>

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+// Import Halfmoon CSS
+import "halfmoon/css/cores/halfmoon.cores.css";
+import "halfmoon/css/halfmoon.min.css";
+
+import './olmain';
+
+// some script here
+const popoverTriggerList = document.querySelectorAll(
+    "[data-bs-toggle='popover']"
+);
+const popoverList = [...popoverTriggerList].map(
+    popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl)
+);
