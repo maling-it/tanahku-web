bb1b07978a5b5458814a1e05a123da3496f7735c
Memet Zx <zxce3@gnuweeb.org>
2024-02-25 00:29:58 +0700

    Add AirTemperature model for retrieving all data
    Signed-off-by: Memet Zx <zxce3@gnuweeb.org>

---
+++ app/Models/AirTemperature.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\Http;
+
+class AirTemperature
+{
+    protected $apiUrl;
+
+    public function __construct()
+    {
+        $this->apiUrl = env('API_URL') . '/api/v1/airtemperature';
+    }
+
+    public function getAllData($deviceId, $pageSize = 100)
+    {
+        $response = Http::get("{$this->apiUrl}/alldata", [
+            'deviceId' => $deviceId,
+            'pageSize' => $pageSize
+        ]);
+
+        return $response->json();
+    }
+}
