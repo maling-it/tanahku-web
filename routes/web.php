896d1b44267e348170a4b73d692085066e12eab3
Memet Zx <zxce3@gnuweeb.org>
2024-02-24 13:47:53 +0700

    add route for device index
    ::
    
    Signed-off-by: Memet Zx <zxce3@gnuweeb.org>

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/device', [DeviceController::class, 'index']);
 Route::get('/device/{id}', [DeviceController::class, 'show']);
