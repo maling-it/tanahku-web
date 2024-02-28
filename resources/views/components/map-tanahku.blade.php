26147a12ae03d86a1ee08404bdc1f7b82ec37ddd
Memet Zx <zxce3@gnuweeb.org>
2024-02-28 22:09:10 +0700

    Update map-tanahku.blade.php styling
    Signed-off-by: Memet Zx <zxce3@gnuweeb.org>

--- resources/views/components/map-tanahku.blade.php
+++ resources/views/components/map-tanahku.blade.php
@@ -7,7 +7,7 @@
     #dot {
         width: 10px;
         height: 10px;
-        border: 1px solid #088;
+        border: 1px solid rgb(0, 0, 0);
         border-radius: 5px;
         background-color: #0FF;
         opacity: 0.5;
@@ -21,6 +21,7 @@
         width: 10px;
         border-radius: 50%;
         display: inline-block;
+        border: 1px solid;
     }
 </style>
 <script>
