0ca33aac13d84e12c8a0065cb9a3e8b2ec7f997f
Memet Zx <zxce3@gnuweeb.org>
2024-03-12 02:28:40 +0700

    update about pages
    idk
    
    Signed-off-by: Memet Zx <zxce3@gnuweeb.org>

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,15 +1,11 @@
 <x-app-layout>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 card m-2 p-3">
-                <h1 class="card-title text-center">About</h1>
-                <div class="text-center card-body">
-                    <p class="card-text">This is a demo application for the Tanah Ku project.</p>
+    <div class="card m-2 p-2">
+        <h1 class="card-title text-center">About</h1>
+        <div class="text-center card-body">
+            <p class="card-text">This is a demo application for the Tanah Ku project.</p>
 
-                    <p class="card-text">The project is a simple Laravel application that demonstrates the use of the
-                        Laravel framework and the Bootstrap 5 front-end framework.</p>
-                </div>
-            </div>
+            <p class="card-text">The project is a simple Laravel application that demonstrates the use of the
+                Laravel framework and the Bootstrap 5 front-end framework.</p>
         </div>
     </div>
 </x-app-layout>
